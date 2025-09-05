be2caac31acfc463db1c3e1fa6f4b3b1cf6f6088
Ishaan <ishaan.mandla@gmail.com>
2025-09-05 19:25:46 +0530

    updated

---
+++ Backend/src/routes/conference.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createConference,
+  getAllConferences,
+  getConferenceById,
+  updateConference,
+  deleteConference,
+} from "../controllers/conference.controllers.js";
+
+const router = express.Router();
+
+router.post("/", createConference);
+router.get("/", getAllConferences);
+router.get("/:id", getConferenceById);
+router.put("/:id", updateConference);
+router.delete("/:id", deleteConference);
+
+export default router;
